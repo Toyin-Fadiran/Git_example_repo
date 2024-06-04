6ac78c8f0e78d68864b8ad2b510faa8b364abe8a
Toyin-Fadiran <toyin.fadiran@gmail.com>
2024-06-03 22:15:35 -0500

    pushing changes to remote remo-- starting Lesson 9

--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -1,11 +1,11 @@
 import { HousingLocationComponent } from "./housing-location/housing-location.component";
 
 export interface HousingLocation {
-    id: number;
+    id: number; // # eg 1
     name: string; //"John Doe", 'John Doe'
     city: string;
     state: string;
-    photo: string; 
+    photo: string; //url of house photo
     availableUnits: number; //1, 4.5
     wifi: boolean;
     laundry: boolean; //false or true
